e65e37b3b3d404b8c03f3688a235fbf90ad8fc02
yatskevich90 <yatskevich90@inbox.ru>
2022-11-18 14:21:58 +0300

    create new six fileas

---
+++ Hw_5.py
@@ -0,0 +1,35 @@
+# [1,2,'a','b'] == [1, 2, 3]    -> 1 + 4 + 9 = 14
+from typing import Tuple, List, Any
+
+
+def filter_list(l: list) -> list:
+    list_nums = [num for num in l if type(num) == int]
+    return list_nums
+print(filter_list([1,2,3, 'a','b']))
+
+
+def bar(n: list) -> tuple[list[Any], int]:
+    result = []
+    sum = 0
+    for i in n:
+        tmp = i**2
+        result.append(tmp)
+        sum += tmp
+    return result, sum
+
+
+print(bar(filter_list([1, 2, 3, 'a', 'b'])))
+
+'''Дана строка, состоящая из слов, вернуть длину кратчайшего слова(слов).
+Проверьте, если строка имеет одинаковое количество " х " и "о". Метод должен возвращать логическое True/False если количесвто разное. Без учета регистра.
+Примеры строк:
+XO("ooxx") => true '''
+
+
+def get_equal(array: str) -> bool:
+    if array.lower().count("x") == array.lower().count("o"):
+        return True
+    else:
+        return False
+
+print(get_equal("ooxXm"))
