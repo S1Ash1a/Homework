a1cc81fd50aa5e10ca8a54387f264067981ffeb9
yatskevich90 <yatskevich90@inbox.ru>
2022-11-24 16:30:25 +0300

    The Next Palindrome number

--- hw_8.py
+++ hw_8.py
@@ -6,3 +6,12 @@ def line(text):
     return s[0] + ''.join(i.capitalize() for i in s[1:])
 print(line("the-stealth-warrior"))
 print(line("The_Stealth_Warrior"))
+
+def next_palin(n):
+    while True:
+        n += 1
+        if str(n) == str(n)[::-1]:
+            return n
+
+n = int(input("Введите число: "))
+print(next_palin(n))
