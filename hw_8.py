ed1e1b9a20e86c3096956d35c9faea82a34043c7
yatskevich90 <yatskevich90@inbox.ru>
2022-11-24 11:21:17 +0300

    CamelCase

--- hw_8.py
+++ hw_8.py
@@ -1,15 +1,9 @@
 def line(text):
-  text_arr = list(text)
-  i = 0
-  while i < len(text_arr):
-    if text_arr[i] == '-' or text_arr[i] == '_':
-      del text_arr[i]
-      if i != len(text_arr) - 1 and text_arr[i].isalpha():
-        text_arr[i] = text_arr[i].upper()
-        i += 1
-    else:
-      i += 1
-  return ''.join(text_arr)
+    s = text.replace("-", " ").replace("_", " ")
+    s = s.split()
+    if len(text) == 0:
+        return text
+    return s[0] + ''.join(i.capitalize() for i in s[1:])
 
 print(line("the-stealth-warrio"))
 print(line("The_Stealth_Warrior"))
