dd5076ed5fe9b7cb55365e2af1eb233459a24a7e
yatskevich90 <yatskevich90@inbox.ru>
2022-11-24 16:25:30 +0300

    Write a function that will convert any string passed to it to CamelCase

---
+++ hw_8.py
@@ -0,0 +1,8 @@
+def line(text):
+    s = text.replace("-", " ").replace("_", " ")
+    s = s.split()
+    if len(text) == 0:
+        return text
+    return s[0] + ''.join(i.capitalize() for i in s[1:])
+print(line("the-stealth-warrior"))
+print(line("The_Stealth_Warrior"))
