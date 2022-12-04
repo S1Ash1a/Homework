a3724dcaf77c511184413bfbc4a73fbbecb0e931
yatskevich90 <yatskevich90@inbox.ru>
2022-12-04 20:55:02 +0300

    Triangle

--- hw_11.py
+++ hw_11.py
@@ -3,7 +3,11 @@ class Triangle:
     def __init__(self, array):
         self.array = array
 
-    def new_triangle(self, a, b, c):
+    def new_triangle(self):
+        a = self.array[0]
+        b = self.array[1]
+        c = self.array[2]
+
         if a + b > c \
                 and a + c > b \
                 and b + c > a:
@@ -12,5 +16,5 @@ class Triangle:
             print("Треугольник не существует")
 
 
-triangle_1 = Triangle("a, b, c")
-triangle_1.new_triangle(a=3, b=2, c=4)
+triangle_1 = Triangle([3, 4, 5])
+triangle_1.new_triangle()
