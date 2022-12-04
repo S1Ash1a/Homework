b19ada67f2d74f2fcc70633031339da5fce54acf
yatskevich90 <yatskevich90@inbox.ru>
2022-12-04 17:21:40 +0300

    Triangle

--- hw_11.py
+++ hw_11.py
@@ -1,19 +1,16 @@
-class Triangle:
-    def __init__(self, name, side_a, side_b, side_c):
-        self.name = name
-        self.side_a = side_a
-        self.side_b = side_b
-        self.side_c = side_c
 
-    def new_triangle(self):
-        if self.side_a + self.side_b > self.side_c \
-                and self.side_a + self.side_c > self.side_b \
-                and self.side_b + self.side_c > self.side_a:
+class Triangle:
+    def __init__(self, name):
+        self.name = name
+
+    def new_triangle(self, a, b, c):
+        if a + b > c \
+                and a + c > b \
+                and b + c > a:
             print("Треугольник существует")
         else:
             print("Треугольник не существует")
 
 
-triangle_1 = Triangle("triangle_1", 4, 5, 7)
-triangle_1.new_triangle()
-
+triangle_1 = Triangle("triangle_1")
+triangle_1.new_triangle(a=3, b=2, c=4)
