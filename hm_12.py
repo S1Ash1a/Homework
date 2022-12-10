2f251d8135cbeadfbf8d423f44ec618a2a77d253
yatskevich90 <yatskevich90@inbox.ru>
2022-12-10 19:10:43 +0300

    Soda

---
+++ hm_12.py
@@ -0,0 +1,19 @@
+class Soda:
+    def __init__(self, additive= None):
+        self.additive = additive
+
+    def show_my_drink(self):
+        if self.additive:
+            print(f"Газировка и {self.additive}")
+        else:
+            print(f"Обычная газировка")
+
+soda_1 = Soda("Pepsi")
+soda_2 = Soda()
+soda_3 = Soda("Orange")
+soda_1.show_my_drink()
+soda_2.show_my_drink()
+soda_3.show_my_drink()
+
+
+
