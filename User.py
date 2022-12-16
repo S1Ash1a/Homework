bf8f5c1a7365998f39cd653ccd5e8eceff4709da
yatskevich90 <yatskevich90@inbox.ru>
2022-12-16 21:01:35 +0300

    User

--- User.py
+++ User.py
@@ -1,33 +1,39 @@
 class User:
-    def __init__(self, name: str, surname: str, age: int, gender: str, profession: str):
+    def __init__(self, name: str, surname: str, age: int, gender: str, profession):
         self.name = name
         self.surname = surname
         self.age = age
         self.gender = gender
-        self.email = name + '.' + surname + '@gmail.com'
         self.profession = profession
-        self.brith_year = 2022 - self.age
-
-
-    def Policeman():
-        return User(name='Peter', surname="Siyu", age= 20, gender='Male', profession="Policeman")
-
-
-    def Doctor():
-        return User(name='Sasha', surname="Ptyshkina", age= 43, gender='Female', profession="Doctor")
-
-    def Teacher():
-        return User(name='Sasha', surname="Ivanov", age= 32, gender='Male', profession="Teacher")
 
 
 
-    def __str__(self):
-        return f"{self.profession}, {self.email}, {self.brith_year}"
+    def new_brith_year(self):
+        britn_year = 2022 - self.age
+        return britn_year
+
+    def get_email(self):
+        email = self.name + '.' + self.surname + '@gmail.com'
+        return email
+
+    def policeman(self):
+        profession = self.profession
+        return profession
+
+    def doctor(self):
+        profession = self.profession
+        return profession
 
 
-person = User(name='Peter', surname="Siyu", age= 20, gender='Male', profession="policeman")
-person_2 = User(name='Sasha', surname="Ptyshkina", age= 43, gender='Female', profession="policeman")
-person_3 = User(name='Sasha', surname="Ivanov", age= 32, gender='Male', profession="policeman")
-print(person)
-print(person_2)
-print(person_3)
+    def teacher(self):
+        profession = self.profession
+        return profession
+
+
+
+person = User(name='Peter', surname="Siyu", age= 20, gender='Male', profession="Policeman")
+person_2 = User(name='Sasha', surname="Ptyshkina", age= 43, gender='Female', profession="Doctor")
+person_3 = User(name='Sasha', surname="Ivanov", age= 32, gender='Male', profession="Teacher")
+print(person.policeman(), person.new_brith_year(), person.get_email())
+print(person_2.doctor(), person_2.new_brith_year(), person_2.get_email())
+print(person_3.teacher(), person_3.new_brith_year(), person_3.get_email())
