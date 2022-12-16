252c3653deb9ed2ff2926aedaf08a721034e994c
yatskevich90 <yatskevich90@inbox.ru>
2022-12-16 22:24:57 +0300

    User

--- User.py
+++ User.py
@@ -1,10 +1,9 @@
 class User:
-    def __init__(self, name: str, surname: str, age: int, gender: str, profession):
+    def __init__(self, name: str, surname: str, age: int, gender: str):
         self.name = name
         self.surname = surname
         self.age = age
         self.gender = gender
-        self.profession = profession
 
 
     def new_brith_year(self):
@@ -15,24 +14,24 @@ class User:
         email = self.name + '.' + self.surname + '@gmail.com'
         return email
 
-    def policeman(self):
-        profession = self.profession
+    def policeman():
+        profession = 'Policeman'
         return profession
 
-    def doctor(self):
-        profession = self.profession
-        return profession
-
-
-    def teacher(self):
-        profession = self.profession
+    def doctor():
+        profession = 'Doctor'
         return profession
 
 
+    def teacher():
+        profession = "Teacher"
+        return profession
 
-person = User(name='Peter', surname="Siyu", age= 20, gender='Male', profession="Policeman")
-person_2 = User(name='Sasha', surname="Ptyshkina", age= 43, gender='Female', profession="Doctor")
-person_3 = User(name='Sasha', surname="Ivanov", age= 32, gender='Male', profession="Teacher")
-print(person.policeman(), person.new_brith_year(), person.get_email())
-print(person_2.doctor(), person_2.new_brith_year(), person_2.get_email())
-print(person_3.teacher(), person_3.new_brith_year(), person_3.get_email())
+
+
+person = User(name='Peter', surname="Siyu", age= 20, gender='Male')
+person_2 = User(name='Sasha', surname="Ptyshkina", age= 43, gender='Female')
+person_3 = User(name='Sasha', surname="Ivanov", age= 32, gender='Male')
+print(User.policeman(), person.new_brith_year(), person.get_email())
+print(User.doctor(), person_2.new_brith_year(), person_2.get_email())
+print(User.teacher(), person_3.new_brith_year(), person_3.get_email())
