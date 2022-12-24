9e161fe6670b81a710f13542b56fc89c3d9e4b00
yatskevich90 <yatskevich90@inbox.ru>
2022-12-24 19:57:41 +0300

    Snow

--- Snow.py
+++ Snow.py
@@ -15,20 +15,13 @@ class Snow:
     def __truediv__(self, n):
         return self.quantity // n
 
-    def __call__(self, new_quantity):
-        return self.quantity == new_quantity
-
     def makeSnow(self, quantity_of_snowflakes):
-        string_snowflakes = ""
         quantity_of_row = self.quantity // quantity_of_snowflakes
         for i in range(quantity_of_row):
-            string_snowflakes += ("*" * quantity_of_snowflakes)
-            string_snowflakes += "\n"
-        rest_of_snowflakes = (self.quantity - quantity_of_row * quantity_of_snowflakes)
-        string_snowflakes += "*" * rest_of_snowflakes
-        return string_snowflakes
+            print("*" * quantity_of_snowflakes)
+        print(self.quantity % quantity_of_snowflakes * '*')
 
-squantity_1 = Snow(200)
-print(squantity_1.makeSnow(10))
+squantity_1 = Snow(14)
+print(squantity_1.makeSnow(3))
 
 
